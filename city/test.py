009de0c996b63b1e7770a0b787bb65df6419fe7e
tianyuchong <854024109@qq.com>
2017-05-26 17:34:30 +0800

    处理下url

--- city/test.py
+++ city/test.py
@@ -1,7 +1,12 @@
 #! python
 # coding=utf-8
 
-import sys,os
+import sys,os,urllib
 sys.path.append(os.path.dirname(os.getcwd()))
-from models.hospital import *
-print Hospital.__tablename__
+url = 'http://map.baidu.com/?newmap=1&reqflag=pcmap&biz=1&from=webmap&da_par=after_baidu&pcevaname=pc4.1&qt=spot' \
+      '&from=webmap&c=268&wd=医院&wd2=&pn=%s&nn=%s&db=0&sug=0&addr=0&pl_data_type=hospital&pl_sub_type=医院&' \
+      'pl_price_section=0,+&pl_sort_type=&pl_sort_rule=0&pl_discount2_section=0,+&pl_groupon_section=0,+&' \
+      'pl_cater_book_pc_section=0,+&pl_hotel_book_pc_section=0,+&pl_ticket_book_flag_section=0,+&pl_movie_book_section=0,+&' \
+      'pl_business_type=hospital&pl_business_id=&da_src=pcmappg.poi.page&on_gel=1&src=7&gr=3&l=12&rn=50&tn=B_NORMAL_MAP&' \
+      'u_loc=12647350,4112147&ie=utf-8&b=(12611382,4109971;12708790,4141395)&t=1494678543176'
+print url % (1, 1 * 10)
--- city/zhengzhou.py
+++ city/zhengzhou.py
@@ -10,31 +10,33 @@ reload(sys)
 
 sys.setdefaultencoding("utf-8")
 url = 'http://map.baidu.com/?newmap=1&reqflag=pcmap&biz=1&from=webmap&da_par=after_baidu&pcevaname=pc4.1&qt=spot&' \
-      'from=webmap&c=268&wd=%E5%8C%BB%E9%99%A2&wd2=&pn=0&nn=0&db=0&sug=0&addr=0&pl_data_type=hospital&' \
+      'from=webmap&c=268&wd=%E5%8C%BB%E9%99%A2&wd2=&pn=%s&nn=%s&db=0&sug=0&addr=0&pl_data_type=hospital&' \
       'pl_sub_type=%E5%8C%BB%E9%99%A2&pl_price_section=0%2C%2B&pl_sort_type=&pl_sort_rule=0&' \
       'pl_discount2_section=0%2C%2B&pl_groupon_section=0%2C%2B&pl_cater_book_pc_section=0%2C%2B&' \
       'pl_hotel_book_pc_section=0%2C%2B&pl_ticket_book_flag_section=0%2C%2B&pl_movie_book_section=0%2C%2B&' \
       'pl_business_type=hospital&pl_business_id=&da_src=pcmappg.poi.page&on_gel=1&src=7&gr=3&l=12&rn=50&' \
       'tn=B_NORMAL_MAP&u_loc=12647350,4112147&ie=utf-8&b=(12611382,4109971;12708790,4141395)&t=1494678543176'
 
-response = urllib2.urlopen(url)
-html = response.read()
-jsonStr = json.loads(html)
-query = session.query(Hospital)
-for item in jsonStr["content"]:
-    # for k, v in item.items():
-    #     print k, "\t", v, "\n"
-    # break
-    if "name" not in item.keys():
-        continue
-    name = item["name"]
-    print name
-    if query.filter(Hospital.name == name).first():
-        continue
-    address = item["addr"] if "addr" in item.keys() else ""
-    # 将医院的信息存储入库
-    hospital = Hospital()
-    hospital.name = name
-    hospital.address = address
-    session.add(hospital)
-    session.commit()
+for i in range(0, 200):
+    print url % (i, i * 10)
+# response = urllib2.urlopen(url)
+# html = response.read()
+# jsonStr = json.loads(html)
+# query = session.query(Hospital)
+# for item in jsonStr["content"]:
+#     # for k, v in item.items():
+#     #     print k, "\t", v, "\n"
+#     # break
+#     if "name" not in item.keys():
+#         continue
+#     name = item["name"]
+#     print name
+#     if query.filter(Hospital.name == name).first():
+#         continue
+#     address = item["addr"] if "addr" in item.keys() else ""
+#     # 将医院的信息存储入库
+#     hospital = Hospital()
+#     hospital.name = name
+#     hospital.address = address
+#     session.add(hospital)
+#     session.commit()
