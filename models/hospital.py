2c22a1f86337f2a955d7a65fc2a315f4bc740848
tianyunchong <854024109@qq.com>
2017-06-29 22:56:45 +0800

    调整下数据接收脚本

--- models/hospital.py
+++ models/hospital.py
@@ -3,12 +3,19 @@
 from sqlalchemy import Column
 from sqlalchemy.types import CHAR, Integer, String, VARCHAR
 from models.baseModel import BaseModel
+from models.baseModel import session
 
 
 class Hospital(BaseModel):
-
     __tablename__ = 'hospital'
 
     id = Column(Integer, primary_key=True)
     name = Column(VARCHAR(255))
     address = Column(VARCHAR(255))
+
+    def exist(self, name):
+        '''check info is exist'''
+        res = session.query(self.__class__).filter(self.__class__.name == name).first()
+        if res:
+            return True
+        return False
